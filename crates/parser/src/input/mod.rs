758a85cc61c90e34f154ba6655bda8b83285b023
GoogleFeud <volen.sl666@gmail.com>
2022-06-24 20:19:31 +0300

    Line consumer

--- crates/parser/src/input/mod.rs
+++ crates/parser/src/input/mod.rs
@@ -31,7 +31,59 @@ impl<'a> LineConsumer<'a> {
             None
         } else {
             self.col += 1;
-            
+            Some(self.data[self.line].as_bytes()[self.col] as char)
         }
     }
+
+    pub fn skip_line(&mut self) {
+        self.line += 1;
+    }
+
+    pub fn skip_char(&mut self) {
+        self.col += 1;
+    }
+
+    pub fn skip_until(&mut self, thing: &str) {
+        for ind in self.line..self.data.len() {
+            if let Some(index) = self.data[ind].find(thing) {
+                self.col = index;
+                break;
+            }
+            self.line += 1;
+        }
+    }
+
+    // Only goes to the end of the line
+    pub fn consume_until_inline(&mut self, thing: &str) -> Option<&str> {
+        if let Some(index) = self.data[self.line].find(thing) {
+            let captured = &self.data[self.line][self.col..index];
+            self.col = index;
+            Some(captured)
+        } else {
+            None
+        }
+    }
+
+    // Checks many lines, but skips line once it finds the thing.
+    pub fn consume_until_block(&mut self, thing: &str) -> String {
+        let mut result = String::new();
+        for ind in self.line..self.data.len() {
+            if let Some(index) = self.data[ind].find(thing) {
+                result += &self.data[ind][0..index];
+                self.col = 0;
+                self.line = ind + 1;
+            }
+        }
+        result
+    }
+
+    pub fn loc(&self) -> Location {
+        Location { col: self.col, line: self.line }
+    }
+
+    pub fn range_here(&self, start: Location) -> Range {
+        Range { start, end: self.loc() }
+    }
+    
+    
 }
