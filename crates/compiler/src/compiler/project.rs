84c081dd6cdfa701e7e4b2adc634dc551f8f53e6
GoogleFeud <volen.sl666@gmail.com>
2022-07-05 21:36:33 +0300

    Start work on compiler

---
+++ crates/compiler/src/compiler/project.rs
@@ -0,0 +1,48 @@
+
+use std::collections::HashMap;
+use crate::files::{file_host::FileHost};
+
+pub trait CompilerContext {
+    fn set_magic_var(&mut self, name: &str, value_kind: MagicVariableType);
+    fn get_magic_var(&self, name: &str) -> Option<&MagicVariableType>;
+    fn get_all_magic_vars(&self) -> Vec<&String>;
+}
+
+pub enum MagicVariableType {
+    String,
+    Number,
+    Array,
+    Map
+}
+
+pub struct Project<T: FileHost> {
+    pub cwd: String,
+    pub host: T,
+    pub magic_variables: HashMap<String, MagicVariableType>
+}
+
+impl<T: FileHost> Project<T> {
+
+    pub fn new(cwd: &str, host: T) -> Self {
+        Self {
+            magic_variables: HashMap::new(),
+            cwd: cwd.to_string(),
+            host
+        }
+    }
+
+}
+
+impl<T: FileHost> CompilerContext for Project<T> {
+    fn set_magic_var(&mut self, name: &str, value_kind: MagicVariableType) {
+        self.magic_variables.insert(name.to_string(), value_kind);
+    }
+
+    fn get_all_magic_vars(&self) -> Vec<&String> {
+        self.magic_variables.keys().collect()
+    }
+
+    fn get_magic_var(&self, name: &str) -> Option<&MagicVariableType> {
+        self.magic_variables.get(name)
+    }
+}
