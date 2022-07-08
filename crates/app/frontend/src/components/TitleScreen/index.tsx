60cde70bae345cb46a33a583525eed2944151b14
GoogleFeud <volen.sl666@gmail.com>
2022-07-08 18:13:41 +0300

    Better layout

--- crates/app/frontend/src/components/TitleScreen/index.tsx
+++ crates/app/frontend/src/components/TitleScreen/index.tsx
@@ -1,17 +1,18 @@
 
 
 export const TitleScreen = () => {
-    return <div class="flex">
-        <div class="w-[80%] bg-[#e8f5e9]">
-            <p>Text...</p>
+    return <div class="flex flex-col h-full">
+        <div class="flex h-[50px] items-center p-8 shadow-[0px_4px_20px_-1px_rgba(0,0,0,0.32)] z-10">
+            <h3 class="text-2xl font-bold">Storytell</h3>
         </div>
-        <div class="bg-[#a1887f] h-[100vh] w-[20%]">
-            <div class="flex flex-col items-center gap-8 mt-[30%]">
-                <h1 class="text-6xl font-bold">Storytell</h1>
-                <div class="font-semibold flex flex-col gap-4">
-                    <p>New Project</p>
-                    <p>Load Project</p>
+        <div class="bg-[#F2F3F4] h-[100vh] flex">
+            <div class="bg-white h-full w-[250px]">
+                <div>
+                    <p>Stories</p>
                 </div>
+            </div>
+            <div>
+                This is a test
             </div>
         </div>
     </div>
