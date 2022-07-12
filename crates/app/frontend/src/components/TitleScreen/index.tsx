960eaca6e5869878d4f21c1e1329a13f319dc70d
GoogleFeud <volen.sl666@gmail.com>
2022-07-12 23:13:15 +0300

    Update design

--- crates/app/frontend/src/components/TitleScreen/index.tsx
+++ crates/app/frontend/src/components/TitleScreen/index.tsx
@@ -1,19 +1,32 @@
 
 
 export const TitleScreen = () => {
-    return <div class="flex flex-col h-full">
-        <div class="flex h-[50px] items-center p-8 shadow-[0px_4px_20px_-1px_rgba(0,0,0,0.32)] z-10">
-            <h3 class="text-2xl font-bold">Storytell</h3>
-        </div>
-        <div class="bg-[#F2F3F4] h-[100vh] flex">
-            <div class="bg-white h-full w-[250px]">
+    return <div class="flex">
+        <div class="p-10 bg-[#E4DCCF] flex flex-col gap-28">
+            <div>
+                <img src="./assets/images/book.png" height="164px" width="152px" />
+                <p class="text-[40px]">Storytell</p>
+            </div>
+            <div class="flex flex-col gap-[38px]">
+                <div class="bg-[#F0EBE3] p-1 mr-[-90px] cursor-pointer">
+                    <p class="text-[20px]">Stories</p>
+                </div>
                 <div>
-                    <p>Stories</p>
+                    <p class="text-[20px] p-1 cursor-pointer">Settings</p>
+                </div>
+                <div>
+                    <p class="text-[20px] p-1 cursor-pointer">Guides</p>
+                </div>
+                <div>
+                    <p class="text-[20px] p-1 cursor-pointer">Exit</p>
+                </div>
+                <div>
+                    <p class="text-[20px] p-1 cursor-pointer">Exit</p>
                 </div>
             </div>
-            <div>
-                This is a test
-            </div>
+        </div>
+        <div class="w-[100vw] bg-[#F0EBE3]">
+
         </div>
     </div>
 }
