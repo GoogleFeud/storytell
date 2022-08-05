5762ca17a85a5486862b3c2a1766b7a494d436cf
GoogleFeud <volen.sl666@gmail.com>
2022-08-05 23:15:25 +0300

    Parse expression

--- crates/js_parser/src/parser/mod.rs
+++ crates/js_parser/src/parser/mod.rs
@@ -1,20 +1,43 @@
 
-use crate::tokenizer::Tokenizer;
+use crate::tokenizer::{Tokenizer, TokenKind};
+use storytell_diagnostics::{diagnostic::*, *};
+use self::ast::*;
 pub mod ast;
 
+make_diagnostics!(define [
+    UNKNOWN_TOKEN,
+    JSP2001,
+    "Unknown token $."
+]);
+
 pub struct JsParser<'a> {
-    pub tokens: Tokenizer<'a>
+    pub tokens: Tokenizer<'a>,
+    pub errors: Vec<Diagnostic>
 }
 
 impl<'a> JsParser<'a> {
 
     pub fn new(content: &'a str) -> Self {
         Self { 
-            tokens: Tokenizer::new(content)
+            tokens: Tokenizer::new(content),
+            errors: vec![]
         }
     }
 
-    
+    fn parse_single_expression(&mut self) -> Option<ASTExpression> {
+        let token = self.tokens.next()?;
+        match token.kind {
+            TokenKind::String => Some(ASTExpression::String(ASTString { range: token.range })),
+            TokenKind::Number => Some(ASTExpression::Number(ASTNumber { range: token.range })),
+            TokenKind::Identifier => Some(ASTExpression::Identifier(ASTIdentifier { range: token.range })),
+            TokenKind::FalseKeyword | TokenKind::TrueKeyword => Some(ASTExpression::Boolean(ASTBoolean { range: token.range })),
+            _ => {
+                self.errors.push(dia!(UNKNOWN_TOKEN, token.range, self.tokens.input.data.from_range(&token.range)));
+                None
+            }
+        }
+    }
+
 }
 
 
