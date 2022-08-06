4797e220668a0403daec54ffa720cef9fbff6190
GoogleFeud <volen.sl666@gmail.com>
2022-08-06 11:18:51 +0300

    Parse binary ops

--- crates/js_parser/src/parser/mod.rs
+++ crates/js_parser/src/parser/mod.rs
@@ -24,6 +24,46 @@ impl<'a> JsParser<'a> {
         }
     }
 
+    pub fn resolve_prec(token: &TokenKind) -> u8 {
+        match token {
+            TokenKind::StarOp | TokenKind::SlashOp | TokenKind::PercentOp => 12,
+            TokenKind::PlusOp | TokenKind::MinusOp => 11,
+            TokenKind::LessThanOp | TokenKind::LessThanEqualsOp | TokenKind::GreaterThanOp | TokenKind::GreaterThanEqualsOp => 9,
+            TokenKind::EqualsEqualsOp | TokenKind::EqualsEqualsEqualsOp | TokenKind::NotEqualsOp | TokenKind::NotEqualsEqualsOp => 8,
+            TokenKind::AmpersandAmpersandOp => 4,
+            TokenKind::BarBarOp | TokenKind::QuestionQuestionOp => 3,
+            TokenKind::PlusEqualsOp | TokenKind::MinusEqualsOp | TokenKind::SlashEqualsOp | TokenKind::StarEqualsOp | TokenKind::EqualsOp => 2,
+            _ => 0
+        }
+    }
+
+    fn parse_binary(&mut self, left: ASTExpression, left_prec: u8) -> Option<ASTExpression> {
+        let start = self.tokens.input.pos;
+        if let Some(next) = self.tokens.peek() {
+            let right_prec = Self::resolve_prec(&next.kind);
+            if right_prec == 0 {
+                return Some(left);
+            };
+            if right_prec > left_prec {
+                let op_token = self.tokens.next().unwrap();
+                let exp = if let Some(exp) = self.parse_single_expression() {
+                    exp
+                } else { return Some(left) };
+                let right = self.parse_binary(exp, right_prec)?;
+                Some(ASTExpression::Binary(Box::from(ASTBinary {
+                    left: left,
+                    right,
+                    operator: op_token.kind,
+                    range: self.tokens.input.range(start)
+                })))
+            } else {
+                Some(left)
+            }
+        } else {
+            Some(left)
+        }
+    }
+
     fn parse_single_expression(&mut self) -> Option<ASTExpression> {
         let token = self.tokens.next()?;
         match token.kind {
@@ -36,6 +76,10 @@ impl<'a> JsParser<'a> {
                 None
             }
         }
+    }
+
+    fn parse_full_expression(&mut self) -> Option<ASTExpression> {
+        unimplemented!("Not implemented")
     }
 
 }
--- crates/js_parser/src/tokenizer.rs
+++ crates/js_parser/src/tokenizer.rs
@@ -37,7 +37,12 @@ pub enum TokenKind {
     PercentOp, // %
     EqualsEqualsEqualsOp, // ===
     EqualsEqualsOp, // ==
-    EqualsOp,
+    NotEqualsOp, // !=
+    NotEqualsEqualsOp, // !==
+    AmpersandAmpersandOp, // &&
+    BarBarOp, // ||
+    QuestionQuestionOp, // ??
+    EqualsOp, // =
     PlusEqualsOp, // +=
     MinusEqualsOp, // -=
     StarEqualsOp, // *=
@@ -212,7 +217,12 @@ impl<'a> Tokenizer<'a> {
             '=' if self.input.is_next(b'=', 0) && self.input.is_next(b'=', 1) => { self.input.skip_chars(2); TokenKind::EqualsEqualsEqualsOp },
             '=' if self.input.is_next(b'=', 0) => { self.input.skip_chars(1); TokenKind::EqualsEqualsOp },
             '=' => TokenKind::EqualsOp,
+            '!' if self.input.is_next(b'=', 0) && self.input.is_next(b'=', 1) => { self.input.skip_chars(2); TokenKind::NotEqualsEqualsOp },
+            '!' if self.input.is_next(b'=', 0) => { self.input.skip_chars(2); TokenKind::NotEqualsOp },
             '!' => TokenKind::ExclamationOp,
+            '&' if self.input.is_next(b'&', 0) => { self.input.skip_chars(1); TokenKind::AmpersandAmpersandOp },
+            '|' if self.input.is_next(b'|', 0) => { self.input.skip_chars(1); TokenKind::BarBarOp },
+            '?' if self.input.is_next(b'?', 0) => { self.input.skip_chars(1); TokenKind::QuestionQuestionOp },
             '.' if self.input.is_next(b'.', 0) && self.input.is_next(b'.', 1) => { self.input.skip_chars(2); TokenKind::DotDotDotOp },
             '.' => TokenKind::DotOp,
             '>' if self.input.is_next(b'=', 0) => { self.input.skip_chars(1); TokenKind::GreaterThanEqualsOp },
