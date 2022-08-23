bdd6d505a658281ef66545bc348694e10a26212b
GoogleFeud <volen.sl666@gmail.com>
2022-08-23 18:38:32 +0300

    Start work on JSON compiler

---
+++ crates/compiler/src/json_compiler/mod.rs
@@ -0,0 +1,43 @@
+use storytell_diagnostics::diagnostic::StorytellResult;
+use storytell_fs::file_host::FileDiagnostic;
+use storytell_parser::ast::model::ASTHeader;
+use crate::{base::*, visitors::MagicVariableCollectorContext, path::Path};
+
+pub struct JSONCompilerProvider;
+
+impl CompilerProvider for JSONCompilerProvider {
+    type Output = String;
+    type Context = JSONCompilerContext;
+
+    fn compile_header(_file: &ASTHeader, _ctx: &mut Self::Context) -> StorytellResult<Self::Output> {
+        Ok(String::new())
+    }
+}
+
+pub struct JSONCompilerContext {
+    pub magic_variables: MagicVariableCollectorContext,
+    pub diagnostics: Vec<FileDiagnostic>,
+    pub paths: Path
+}
+
+impl CompilerContext for JSONCompilerContext {
+    fn add_diagnostic(&mut self, dia: FileDiagnostic) {
+        self.diagnostics.push(dia);
+    }
+
+    fn get_global_path(&mut self) -> &mut Path {
+        &mut self.paths
+    }
+}
+
+impl JSONCompilerContext {
+
+    pub fn new() -> Self {
+        Self { 
+            magic_variables: MagicVariableCollectorContext::new(),
+            diagnostics: vec![],
+            paths: Path::new("global")
+        }
+    }
+
+}
