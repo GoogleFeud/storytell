946b1c262b064d66e2e96c25486bd05812a2fcaa
GoogleFeud <volen.sl666@gmail.com>
2022-08-24 18:56:03 +0300

    json macro

--- crates/compiler/src/json_compiler/compile.rs
+++ crates/compiler/src/json_compiler/compile.rs
@@ -1,7 +1,25 @@
 use storytell_diagnostics::diagnostic::StorytellResult;
 use storytell_parser::ast::model::*;
+use std::stringify;
 
 use crate::json_compiler::JSONCompilerContext;
+
+macro_rules! strip_plus {
+    (+ $($rest: tt)*) => {
+        $($rest)*
+    }
+}
+
+macro_rules! json {
+    {$($property: ident: $value: expr),+} => {
+        strip_plus!($(
+            +format!("\"{}\":{}", stringify!($property), $value.safe_compile())
+        )+)
+    },
+    ($value: expr) => {
+        $value.safe_compile()
+    }
+}
 
 pub trait JSONCompilable {
     fn compile(&self, ctx: &mut JSONCompilerContext) -> StorytellResult<String>;
@@ -9,4 +27,35 @@ pub trait JSONCompilable {
 
 pub trait JSONSafeCompilable {
     fn safe_compile(&self) -> String;
+}
+
+
+impl JSONSafeCompilable for String {
+    fn safe_compile(&self) -> String {
+        format!("\"{}\"", self)
+    }
+}
+
+impl<T: JSONSafeCompilable> JSONSafeCompilable for Vec<T> {
+    fn safe_compile(&self) -> String {
+        format!("[{}]", self.iter().map(|i| i.safe_compile()).collect::<Vec<String>>().join(","))
+    }
+}
+
+impl JSONSafeCompilable for u32 {
+    fn safe_compile(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl JSONSafeCompilable for i32 {
+    fn safe_compile(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl JSONSafeCompilable for &str {
+    fn safe_compile(&self) -> String {
+        format!("\"{}\"", self)
+    }
 }
