1917af1abe9f59c9f02e3dc4d9182e7d30de670f
GoogleFeud <volen.sl666@gmail.com>
2022-09-05 11:39:13 +0300

    Color change on hover

--- crates/app/frontend/src/components/Input/Button.tsx
+++ crates/app/frontend/src/components/Input/Button.tsx
@@ -4,7 +4,7 @@ export const Button = (props: {
     onClick?: () => void,
     primary?: boolean
 }) => {
-    return <div class={`rounded ${props.primary ? "bg-[#6d4c41]" : "bg-[#404040]"} text-white text-[14px] px-4 py-2 flex justify-center items-center cursor-pointer min-w-[90px]`} onClick={props.onClick}>
+    return <div class={`rounded ${props.primary ? "bg-[#6d4c41] hover:bg-[#8f6657]" : "bg-[#404040] hover:bg-[#595959]"} text-white text-[14px] px-4 py-2 flex justify-center items-center cursor-pointer transition-all min-w-[90px]`} onClick={props.onClick}>
         {props.text}
     </div>;
 };
--- crates/app/frontend/src/components/Input/TextField.tsx
+++ crates/app/frontend/src/components/Input/TextField.tsx
@@ -6,7 +6,7 @@ export const TextField = (props: {
     icon?: JSXElement,
     placeholder?: string
 }) => {
-    return <div class="text-[13px] p-2 border rounded border-neutral-700 outline-1 flex items-center gap-3 focus-within:bg-[#282626] focus-within:border-neutral-500 transition-all">
+    return <div class="text-[13px] p-2 border rounded border-neutral-700 outline-1 flex items-center gap-3 focus-within:bg-[#282626] hover:bg-[#282626] focus-within:border-neutral-500 transition-all">
         {props.icon}
         <input type="text" class="bg-transparent outline-0" placeholder={props.placeholder} onChange={(ctx) => props.onChange?.((ctx.target as HTMLInputElement).value)}></input>
     </div>;
